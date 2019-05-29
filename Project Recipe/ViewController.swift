39652c561b0ad3936c64fa348833885da635cfe0
navianme <navenvijay2011@gmail.com>
2019-05-29 18:54:24 -0400

    version support to 11.2

--- Project Recipe/ViewController.swift
+++ Project Recipe/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UIViewController
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
         navigationTitle.title = navTitle
         title = navTitle
         
